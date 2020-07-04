4ce1dcb4356278c6a586e403a9b2bf7033b49fdb
juancun2020 <juancun112020@gmail.com>
2020-07-04 13:20:29 -0500

    Diseño vista por categorias del producto 4 julio 2020

--- www/js/app.js
+++ www/js/app.js
@@ -8,104 +8,122 @@
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs).
-    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
-    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
-    // useful especially with forms, though we would prefer giving the user a little more room
-    // to interact with the app.
-    if (window.cordova && window.Keyboard) {
-      window.Keyboard.hideKeyboardAccessoryBar(true);
-    }
+	$ionicPlatform.ready(function() {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs).
+		// The reason we default this to hidden is that native apps don't usually show an accessory bar, at
+		// least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
+		// useful especially with forms, though we would prefer giving the user a little more room
+		// to interact with the app.
+		if (window.cordova && window.Keyboard) {
+		  window.Keyboard.hideKeyboardAccessoryBar(true);
+		}
 
-    if (window.StatusBar) {
-      // Set the statusbar to use the default style, tweak this to
-      // remove the status bar on iOS or change it to use white instead of dark colors.
-      StatusBar.styleDefault();
-    }
-  });
+		if (window.StatusBar) {
+		  // Set the statusbar to use the default style, tweak this to
+		  // remove the status bar on iOS or change it to use white instead of dark colors.
+		  StatusBar.styleDefault();
+		}
+	});
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+	// Ionic uses AngularUI Router which uses the concept of states
+	// Learn more here: https://github.com/angular-ui/ui-router
+	// Set up the various states which the app can be in.
+	// Each state's controller can be found in controllers.js
+	$stateProvider
 
-  //vista Tutorial
+	//Estado vista Tutorial
 
-  .state("tutorial",{
-    url:"/tutorial",
-    templateUrl:"templates/tutorial.html",
-    controller:"tutorialCtrl"
-  })
+	.state("tutorial",{
+		url:"/tutorial",
+		templateUrl:"templates/tutorial.html",
+		controller:"tutorialCtrl"
+	})
 
-  .state("registro",{
-    url:"/registro",
-    templateUrl:"templates/register.html",
-    controller:"registroCtrl"
-  })
+	//Estado vista products mostrar productos al hacer click
 
-  //vista inicio
+	.state("products",{
+		url: "/products",
+		templateUrl: "templates/products.html",
+		controller : "productsCtrl"
+	})
 
-  .state("login",{
-    url: "/login",
-    templateUrl: "templates/login.html",
-    controller:"loginCtrl"
-  })
+	//Estado vista registro
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
+	.state("registro",{
+		url:"/registro",
+		templateUrl:"templates/register.html",
+		controller:"registroCtrl"
+	})
 
-  // Each tab has its own nav history stack:
+	// Estado vista inicio
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
+	.state("login",{
+		url: "/login",
+		templateUrl: "templates/login.html",
+		controller:"loginCtrl"
+	})
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+  	//Estado vista  productos
+	.state("productos",{
+		url: "/productos",
+		templateUrl: "templates/productos.html",
+		controller:"productosCtrl"
+	})
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+  	// setup an abstract state for the tabs directive
+	.state('tab', {
+		url: '/tab',
+		abstract: true,
+		templateUrl: 'templates/tabs.html'
+	})
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tutorial');
+  	// Each tab has its own nav history stack:
+
+	.state('tab.dash', {
+		url: '/dash',
+		views: {
+			'tab-dash': {
+				templateUrl: 'templates/tab-dash.html',
+				controller: 'DashCtrl'
+			}
+		}
+	})
+
+	.state('tab.chats', {
+		url: '/chats',
+		views: {
+			'tab-chats': {
+				templateUrl: 'templates/tab-chats.html',
+				controller: 'ChatsCtrl'
+			}
+		}
+	})
+
+	.state('tab.chat-detail', {
+		url: '/chats/:chatId',
+		views: {
+			'tab-chats': {
+				templateUrl: 'templates/chat-detail.html',
+				controller: 'ChatDetailCtrl'
+			}
+		}
+	})
+
+	.state('tab.account', {
+		url: '/account',
+		views: {
+			'tab-account': {
+				templateUrl: 'templates/tab-account.html',
+				controller: 'AccountCtrl'
+			}	
+		}
+	});
+
+	// if none of the above states are matched, use this as the fallback
+	$urlRouterProvider.otherwise('/tab/dash');
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,103 +1,146 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyCtzD_d6_Rc0SN9NSqXTDcYnjxqcUEYW5g",
-    authDomain: "maxapp-df7ce.firebaseapp.com",
-    databaseURL: "https://maxapp-df7ce.firebaseio.com",
-    projectId: "maxapp-df7ce",
-    storageBucket: "maxapp-df7ce.appspot.com",
-    messagingSenderId: "883374548975",
-    appId: "1:883374548975:web:4f11acaedd03d0e3efb81a",
-    measurementId: "G-PN5E88EJ6Z"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  //base de datos
-  var database = firebase.database();
+	apiKey: "AIzaSyCtzD_d6_Rc0SN9NSqXTDcYnjxqcUEYW5g",
+	authDomain: "maxapp-df7ce.firebaseapp.com",
+	databaseURL: "https://maxapp-df7ce.firebaseio.com",
+	projectId: "maxapp-df7ce",
+	storageBucket: "maxapp-df7ce.appspot.com",
+	messagingSenderId: "883374548975",
+	appId: "1:883374548975:web:4f11acaedd03d0e3efb81a",
+	measurementId: "G-PN5E88EJ6Z"
+	};
+	// Initialize Firebase
+	firebase.initializeApp(firebaseConfig);
+	//base de datos
+	var database = firebase.database();
 
 angular.module('starter.controllers', [])
 
+//Controlador Para registro de usuario
 .controller("registroCtrl",function($scope){
-  $scope.obtener = function(user){
-    firebase.auth().createUserWithEmailAndPassword(user.email, user.contra).then(function a(y){
-      swal("se creo correctamente YEAH")
-        firebase.database().ref("/usuario").set()({
-          correo: user.email
-    })
-    firebase.out().signout().then(function(){
-      // sign-out successful
-    }).catch(function(error){
-      // An error.
-    }); // hasta aqui
-    }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-    });
-  }
+	$scope.obtener = function(user){
+	firebase.auth().createUserWithEmailAndPassword(user.email, user.contra).then(function a(y){
+		swal("se creo correctamente YEAH")
+			firebase.database().ref("/usuario").set()({
+				correo: user.email
+	})
+	firebase.out().signout().then(function(){
+		// sign-out successful
+		}).catch(function(error){
+		// An error.
+		}); // hasta aqui
+		}).catch(function(error) {
+		// Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		// ...
+		});
+	}
 })
 
-//vista inicio
+//Controlador vista inicio
 .controller("loginCtrl",function($scope){
 
 })
 
-//vista principal
+//Controlador vista principal
 
 .controller("tutorialCtrl",function($scope){
 
 })
 
-.controller('DashCtrl', function($scope,$rootScope) {
-  $rootScope.Categorias = [
-  {
-    nombreCategoria : "TV Y VIDEO",
-    imagen : "img/tv.jpg"
-  },
-  {
-    nombreCategoria : "CELULARES",
-    imagen : "img/cel.jpg"
-  },
-  {
-    nombreCategoria : "LINEA BLANCA",
-    imagen : "img/refril.jpg"
-  },
-  {
-    nombreCategoria : "VIDEOJUEGOS",
-    imagen : "img/game.jpg"
-  },
-  {
-    nombreCategoria : "COMPUTACION",
-    imagen : "img/pc.jpg"
-  },
-  {
-    nombreCategoria : "AUDIO",
-    imagen : "img/aud.jpg"
-  }
+//Controlador vista products mostrar productos por categoria filtrados
+
+.controller("productsCtrl",function($scope, $rootScope){
+
+})
+//controlador vista de productos por categoria sin filtrar
+.controller('DashCtrl', function($scope,$rootScope, $state) {
+	$rootScope.listaProductos=[];
+	$rootScope.lista = [];
+	firebase.database().ref("/productos").on("value", function(p){
+		$rootScope.listaProductos = p.val();
+
+	p.forEach(function(datos){
+		$rootScope.lista.push(datos.val());
+	})
+
+		console.log($rootScope.listaProductos);
+	})
+
+	//Diccionario categoria de productos
+	$rootScope.Categorias = [
+		{
+			nombreCategoria : "TV y VIDEO",
+			imagen : "img/tag1.png",
+			descripcion:"Televisores, Audio y Reproductores."
+		},
+		{
+			nombreCategoria : "CELULARES",
+			imagen : "img/tag2.png",
+		 	descripcion:"Tigo, Claro y Liberados."
+		},
+		{
+			nombreCategoria : "LINEA BLANCA",
+			imagen : "img/tag3.png",
+			descripcion:"Refrigeracion, Estufas, Lavadoras."
+		},
+		{
+			nombreCategoria : "VIDEOJUEGOS",
+			imagen : "img/tag4.png",
+			descripcion:"Playstation, Xbox One, Pc Gaming."
+		},
+		{
+			nombreCategoria : "COMPUTACION",
+			imagen : "img/tag5.png",
+			descripcion:"Laptop, Desktop, Accesorios."
+		},
+		{
+			nombreCategoria : "AUDIO",
+			imagen : "img/tag6.png",
+			descripcion:"Audifonos, Fiestas, Bocinas Personales."
+		}
   
-  ]
+	]
+
+	//mostrar productos filtrados por categoria
+	$scope.viewProducts = function(ncategoria){
+		$rootScope.nombreCategoria = ncategoria;
+		$state.go("products")
+	}
+
+})
+
+//nuevo controlador productos por categoria sin filtrar
+.controller("productosCtrl",function($scope, $rootScope, $state){
+
+	firebase.database().ref("/productos").on("value", function(snapshot) {
+		$rootScope.Categorias = snapshot.val()
+		}, function (error) {
+		console.log("Error: " + error.code);
+	});
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+	// With the new view caching in Ionic, Controllers are only called
+	// when they are recreated or on app start, instead of every page change.
+	// To listen for when this page is active (for example, to refresh data),
+	// listen for the $ionicView.enter event:
+	//
+	//$scope.$on('$ionicView.enter', function(e) {
+	//});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+	$scope.chats = Chats.all();
+	$scope.remove = function(chat) {
+		Chats.remove(chat);
+	};
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+	$scope.chat = Chats.get($stateParams.chatId);
 })
 
 .controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+	$scope.settings = {
+		enableFriends: true
+	};
 });
